53a6c3ec1b8f8d7c6d71b4599adac4e37498be2d
MaurizioFranco <maurizio.franco@ymail.com>
2020-03-20 19:03:14 +0100

    getUserRegdateInPeriod method moved from UserRepositoy to CandidateRepository(renamed to getCandidateCountWithRegdateInPeriod)

--- src/main/java/centauri/academy/cerepro/persistence/repository/candidate/CandidateRepository.java
+++ src/main/java/centauri/academy/cerepro/persistence/repository/candidate/CandidateRepository.java
@@ -1,8 +1,10 @@
 package centauri.academy.cerepro.persistence.repository.candidate;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import centauri.academy.cerepro.persistence.entity.Candidate;
 import centauri.academy.cerepro.persistence.entity.CandidateCustom;
@@ -21,5 +23,6 @@ public interface CandidateRepository extends JpaRepository<Candidate, Long>, Can
 	
 	List<Candidate> findByUserId(long userId);
 
-	
+	@Query("select count(can.id) from Candidate can join User us on can.userId = us.id where us.regdate >= :dataStart and us.regdate < :dataEnd")
+	long getCandidateCountWithRegdateInPeriod(LocalDateTime dataStart, LocalDateTime dataEnd);
 }
