fa62cb9ac5fc997297890d3bb3c86f681304509e
MaurizioFranco <maurizio.franco@ymail.com>
2020-03-20 19:04:12 +0100

    added log message to getFakeCandidate method

--- src/test/java/centauri/academy/cerepro/persistence/repository/UserRepositoryTest.java
+++ src/test/java/centauri/academy/cerepro/persistence/repository/UserRepositoryTest.java
@@ -3,11 +3,17 @@ package centauri.academy.cerepro.persistence.repository;
  * @author anna
  */
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,6 +22,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import centauri.academy.cerepro.persistence.entity.User;
@@ -166,5 +174,4 @@ public class UserRepositoryTest extends AbstractRepositoryTest {
 		Page<User> currentPage = ur.findAll(PageRequest.of(1, 5, Sort.Direction.ASC, "email"));
 		assertTrue(currentPage.getNumberOfElements() == 0);
     }
-
 }
