b735d3762b2ec6f36d4e19d2866ea0ca2e138644
MaurizioFranco <maurizio.franco@ymail.com>
2020-05-12 18:29:56 +0200

    Updated dev and test environments

--- resources/sql/01_schema_&_db_users.sql
+++ resources/sql/01_schema_&_db_users.sql
@@ -10,16 +10,4 @@ CREATE USER 'cerepro_test'@'%' IDENTIFIED BY 'cerepro_test';
 GRANT ALL PRIVILEGES ON cerepro_test.* TO 'cerepro_test'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 
-drop database IF EXISTS  cerepro_stage;
-create database cerepro_stage;
-CREATE USER 'cerepro_stage'@'%' IDENTIFIED BY 'cerepro_stage1979';
-GRANT ALL PRIVILEGES ON cerepro_stage.* TO 'cerepro_stage'@'%'  WITH GRANT OPTION;
-FLUSH PRIVILEGES;
-
-drop database IF EXISTS  cerepro_prod;
-create database cerepro_prod;
-CREATE USER 'cerepro_prod'@'%' IDENTIFIED BY 'cerepro_prod1979';
-GRANT ALL PRIVILEGES ON cerepro_prod.* TO 'cerepro_prod'@'%'  WITH GRANT OPTION;
-FLUSH PRIVILEGES;
-
 
