6a6d6ba284abb8bb88f229b62ecab701ee69d49d
MaurizioFranco <maurizio.franco@ymail.com>
2023-02-24 17:15:24 +0100

    Added candidatesurveytokens

--- resources/sql/06_altertable_drop_columns.sql
+++ resources/sql/06_altertable_drop_columns.sql
@@ -4,3 +4,19 @@ alter table users drop column note;
 
 drop table itconsultants ;
 
+create table candidatesurveytokens (
+	id bigint NOT NULL AUTO_INCREMENT, 
+	candidate_id bigint not null,
+	survey_id bigint not null,
+	generated_token VARCHAR(50),
+	expiration_date_time datetime,
+	expired boolean DEFAULT false,
+	primary key(id),
+	CONSTRAINT uniqueToken UNIQUE (generated_token),
+	foreign key (candidate_id) references candidates(id),
+	foreign key (survey_id) references surveys(id)
+);
+
+INSERT INTO candidatesurveytokens (candidate_id, survey_id, generated_token, expiration_date_time, expired)
+select c.id, u.surveyid, u.generatedtoken, u.expirationdate, u.expired from usersurveytoken u, candidates c where u.userid=c.user_id 
+
