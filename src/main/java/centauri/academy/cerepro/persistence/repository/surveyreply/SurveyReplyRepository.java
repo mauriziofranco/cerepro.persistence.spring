47406d126fdfdc9674fe5f472f0e48384f00ea44
DaimCod <daim.kazmi00@gmail.com>
2023-03-09 11:42:05 +0100

    Added method updatePdfFileName

--- src/main/java/centauri/academy/cerepro/persistence/repository/surveyreply/SurveyReplyRepository.java
+++ src/main/java/centauri/academy/cerepro/persistence/repository/surveyreply/SurveyReplyRepository.java
@@ -3,8 +3,12 @@ package centauri.academy.cerepro.persistence.repository.surveyreply;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import centauri.academy.cerepro.persistence.entity.SurveyReply;
@@ -26,6 +30,9 @@ public interface SurveyReplyRepository extends JpaRepository<SurveyReply, Long>,
 	
 	@Query("select count(id) from SurveyReply where endtime >=  :ldtstart AND endtime < :ldtend")
 	long getSurveyReplyCountToday(LocalDateTime ldtstart, LocalDateTime ldtend);
-		 
-
+		
+	@Transactional
+	@Modifying
+	@Query("UPDATE SurveyReply SET pdffilename = :pdfname WHERE id = :id")
+	void updatePdfFileName(@Param("pdfname")String pdfname, @Param("id")long id);
 }
