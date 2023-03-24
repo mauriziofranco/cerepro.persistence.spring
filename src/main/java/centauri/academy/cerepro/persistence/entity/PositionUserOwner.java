fa8f5979b16e5e0af72bf2b7808597f79935a3c8
Giammarco93 <giammarco.lucchetti@hotmail.it>
2023-03-24 09:44:28 +0100

    Fixed Position User Owner lombock

--- src/main/java/centauri/academy/cerepro/persistence/entity/PositionUserOwner.java
+++ src/main/java/centauri/academy/cerepro/persistence/entity/PositionUserOwner.java
@@ -50,34 +50,32 @@ public class PositionUserOwner {
 		this.userId = userId;
 	}
 
-	public PositionUserOwner() {
-		super();
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Long getCoursePageId() {
-		return coursePageId;
-	}
-
-	public void setCoursePageId(Long coursePageId) {
-		this.coursePageId = coursePageId;
-	}
-
-	public Long getUserId() {
-		return userId;
-	}
-
-	public void setUserId(Long userId) {
-		this.userId = userId;
-	}
-	
-	
+//	public PositionUserOwner() {
+//		super();
+//	}
+//
+//	public Long getId() {
+//		return id;
+//	}
+//
+//	public void setId(Long id) {
+//		this.id = id;
+//	}
+//
+//	public Long getCoursePageId() {
+//		return coursePageId;
+//	}
+//
+//	public void setCoursePageId(Long coursePageId) {
+//		this.coursePageId = coursePageId;
+//	}
+//
+//	public Long getUserId() {
+//		return userId;
+//	}
+//
+//	public void setUserId(Long userId) {
+//		this.userId = userId;
+//	}
 	
 }
